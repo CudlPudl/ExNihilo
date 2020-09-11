60e46dc8ef3acaeb9c2d42d8f29e9a48075b8682
Cudl <tossubot@gmail.com>
2020-09-11 23:53:59 +0300

    Move the folder structure one folder up. *rolls eyes*

---
+++ Assets/Scripts/HitScanner.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitScanner : MonoBehaviour
+{
+    // Update is called once per frame
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+
+        }
+    }
+}
---
+++ Assets/Scripts/Plant.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Plant : MonoBehaviour
+{
+    public enum Personality
+    {
+        NONE = 0,
+        Cute = 1,
+        Happy = 2,
+        Sad = 3,
+        Grumpy = 4
+    }
+
+    public class PlantEvent : UnityEvent<Plant> { }
+
+    private float _growth = 0f;
+
+    [SerializeField]
+    private Personality _personality = Personality.Cute;
+    [SerializeField]
+    private Transform _root;
+    [SerializeField]
+    private 
+    [SerializeField]
+    private float MaxGrowthDuration = 5f;
+    [SerializeField]
+    private float MaxSize = 5f;
+
+    public PlantEvent OnPickEvent;
+
+    public float GetPlantSize() => Mathf.Lerp(1, MaxSize, _growth / MaxGrowthDuration);
+    public bool IsFullyGrown() => _growth >= MaxGrowthDuration;
+    public void PickPlant()
+    {
+        if (!IsFullyGrown()) return;
+        OnPickEvent.Invoke(this);
+        Destroy(gameObject);
+    }
+
+    private void Update()
+    {
+        _growth += Time.deltaTime;
+        _root.localScale = new Vector3(1, GetPlantSize(), 1);
+    }
+}
